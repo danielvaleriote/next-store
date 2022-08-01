cc5c2639d42e08d28955106baff2ca41896e07ea
DanielValeriote <danielvaleriote@outlook.com>
2022-08-01 13:02:03 -0300

    regular navbar ready

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,39 @@
 import { useEffect, useState } from 'react';
-import { Header, MobileNavbar, RegularNavbar } from './styled';
+import { useRouter } from 'next/router';
+
+import {
+	Header,
+	MobileNavbar,
+	RegularNavbar,
+	NavItem,
+	NavList,
+} from './styled';
+import Link from 'next/link';
+import { FaShoppingCart } from 'react-icons/fa';
+
+type NavLinkProps = {
+	href: string;
+	children: React.ReactNode;
+	cart?: boolean;
+};
+
+const NavLink = ({ href, children, cart }: NavLinkProps) => {
+	const router = useRouter();
+
+	return (
+		<NavItem
+			style={
+				cart
+					? { position: 'absolute', right: '20px', paddingTop: '2px' }
+					: undefined
+			}
+		>
+			<Link href={href} passHref={true}>
+				<a className={router.pathname == href ? 'active' : ''}>{children}</a>
+			</Link>
+		</NavItem>
+	);
+};
 
 const Navbar = () => {
 	const [isMobile, setIsMobile] = useState(false);
@@ -15,7 +49,18 @@ const Navbar = () => {
 			{isMobile ? (
 				<MobileNavbar></MobileNavbar>
 			) : (
-				<RegularNavbar></RegularNavbar>
+				<RegularNavbar>
+					<NavList>
+						<NavLink href="/">Início</NavLink>
+						<NavLink href="/products/electronics">Eletrônicos</NavLink>
+						<NavLink href="/products/jewelery">Joalheria</NavLink>
+						<NavLink href="/mensclothing">Moda masculinas</NavLink>
+						<NavLink href="/womensclothing">Moda femininas</NavLink>
+						<NavLink href="/user/cart" cart={true}>
+							<FaShoppingCart />
+						</NavLink>
+					</NavList>
+				</RegularNavbar>
 			)}
 		</Header>
 	);
--- src/components/Navbar/styled.ts
+++ src/components/Navbar/styled.ts
@@ -8,6 +8,30 @@ export const Header = styled.header`
 	position: relative;
 `;
 
+export const NavList = styled.ul`
+	display: flex;
+	gap: 20px;
+`;
+
+export const NavItem = styled.li`
+	color: var(--main-white);
+	font-size: 1.1rem;
+	white-space: nowrap;
+	&:first-child {
+		padding-right: 20px;
+	}
+	& a {
+		opacity: 0.7;
+	}
+	& a:hover {
+		opacity: 0.9;
+		text-shadow: 1px 1px 3px rgba(100, 100, 100, 0.8);
+	}
+	& .active {
+		opacity: 1;
+	}
+`;
+
 export const MobileNavbar = styled.nav``;
 
 export const RegularNavbar = styled.nav``;
